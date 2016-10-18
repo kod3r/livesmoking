39893ccf482a60114c32a8dbe5a9891641b8716e
Fabian Aussems <fabianaussems@gmail.com>
2016-10-18 22:29:23 +0200

    messing around

--- moz/project.clj
+++ moz/project.clj
@@ -1,8 +1,16 @@
 (defproject livesmoking "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
-                 [reagent "0.6.0"]
-                 [re-frame "0.8.0"]
+                 [reagent "0.6.0"
+                  :exclusions [[cljsjs/react]
+                               [cljsjs/react-dom]
+                               [cljsjs/react-dom-server]
+                               ]]
+                 [re-frame "0.8.0"
+                  :exclusions [[cljsjs/react]
+                               [cljsjs/react-dom]
+                               [cljsjs/react-dom-server]
+                               ]]
                  [secretary "1.2.3"]]
 
   :plugins [[lein-cljsbuild "1.1.4"]]
